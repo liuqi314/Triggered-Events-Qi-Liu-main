bea5d03820befd961c975307c5a922f51bcb0b1f
liuqi314 <w0490374@nscc.ca>
2023-12-02 22:56:22 -0400

    Modify Timeline 3

---
+++ Assets/Scripts/Trigger3.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Trigger3 : MonoBehaviour
+{
+    public PlayableDirector timeline;
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+
+        if (other.gameObject.tag == "Player1")
+        {
+            timeline.Play();
+
+        }
+
+
+
+    }
+
+
+}
